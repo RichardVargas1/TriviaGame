1b01a0245f379e0a104f8e3f96c541ed70f5f0a6
RichardVargas1 <richard.vargas95@gmail.com>
2018-11-13 20:59:15 -0600

    Changed names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
-var card = $("#quiz-area");
-var countStartNumber = 30;
+var gameBody = $("#quiz-area");
+var timerStart = 30;
 
 // One object holding in my questions for my trivia game.
 var questions = [{
@@ -47,78 +47,77 @@ var questions = [{
 // =========================================================================================================
 // I will come back at a later time to add in some gifs for my questions.
 
-var timer;
+var gameTimer;
 
-var game = {
+var triviaGame = {
 
   questions: questions,
   currentQuestion: 0,
-  counter: countStartNumber,
-  correct: 0,
-  incorrect: 0,
+  counter: timerStart,
+  correctAnswerChoice: 0,
+  incorrectAnswerChoice: 0,
 
   countdown: function() {
-    game.counter--;
-    $("#counter-number").text(game.counter);
+    triviaGame.counter--;
+    $("#counter-number").text(triviaGame.counter);
     if (game.counter === 0) {
-      console.log("TIME UP");
-      game.timeUp();
+      triviaGame.timeUp();
     }
   },
 
   loadQuestion: function() {
 
-    timer = setInterval(game.countdown, 1000);
+    gameTimer = setInterval(triviaGame.countdown, 1000);
 
-    card.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+    gameBody.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
     for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
-      card.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      gameBody.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
       + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
     }
   },
 
   nextQuestion: function() {
-    game.counter = countStartNumber;
-    $("#counter-number").text(game.counter);
-    game.currentQuestion++;
-    game.loadQuestion();
+    triviaGame.counter = timerStart;
+    $("#counter-number").text(triviaGame.counter);
+    triviaGame.currentQuestion++;
+    triviaGame.loadQuestion();
   },
 
   timeUp: function() {
 
-    clearInterval(timer);
+    clearInterval(gameTimer);
 
-    $("#counter-number").html(game.counter);
+    $("#counter-number").html(triviaGame.counter);
 
-    card.html("<h2>Out of Time!</h2>");
-    card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
-    card.append("<img src='" + questions[this.currentQuestion].gif + "' />");
+    gameBody.html("<h2>Out of Time!</h2>");
+    gameBody.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    gameBody.append("<img src='" + questions[this.currentQuestion].gif + "' />");
 
-    if (game.currentQuestion === questions.length - 1) {
-      setTimeout(game.results, 3 * 1000);
+    if (triviaGame.currentQuestion === questions.length - 1) {
+      setTimeout(triviaGame.results, 3 * 1000);
     }
     else {
-      setTimeout(game.nextQuestion, 3 * 1000);
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
     }
   },
 
   results: function() {
 
-    clearInterval(timer);
+    clearInterval(gameTimer);
 
-    card.html("<h2>All done, heres how you did!</h2>");
+    gameBody.html("<h2>Quiz Finished. Here are your results.</h2>");
 
-    $("#counter-number").text(game.counter);
+    $("#counter-number").text(triviaGame.counter);
 
-    card.append("<h3>Correct Answers: " + game.correct + "</h3>");
-    card.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
-    card.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
-    card.append("<br><button id='start-over'>Start Over?</button>");
+    gameBody.append("<h3>Questions You Got Right: " + triviaGame.correctAnswerChoice + "</h3>");
+    gameBody.append("<h3>Questions You Got Wrong: " + triviaGame.incorrectAnswerChoice + "</h3>");
+    gameBody.append("<h3>Unanswered Questions: " + (questions.length - (triviaGame.incorrectAnswerChoice + triviaGame.correctAnswerChoice)) + "</h3>");
+    gameBody.append("<br><button id='start-over'>Start Over?</button>");
   },
 
   clicked: function(e) {
-    clearInterval(timer);
+    clearInterval(gameTimer);
     if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
       this.answeredCorrectly();
     }
@@ -129,44 +128,44 @@ var game = {
 
   answeredIncorrectly: function() {
 
-    game.incorrect++;
+    gtriviaGame.incorrectAnswerChoice++;
 
-    clearInterval(timer);
+    clearInterval(gameTimer);
 
-    card.html("<h2>Nope!</h2>");
-    card.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
-    card.append("<img src='" + questions[game.currentQuestion].gif + "' />");
+    gameBody.html("<h2>Nope!</h2>");
+    gameBody.append("<h3>The Correct Answer was: " + questions[triviaGame.currentQuestion].correctAnswer + "</h3>");
+    gameBody.append("<img src='" + questions[triviaGame.currentQuestion].gif + "' />");
 
-    if (game.currentQuestion === questions.length - 1) {
-      setTimeout(game.results, 3 * 1000);
+    if (triviaGame.currentQuestion === questions.length - 1) {
+      setTimeout(triviaGame.results, 3 * 1000);
     }
     else {
-      setTimeout(game.nextQuestion, 3 * 1000);
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
     }
   },
 
   answeredCorrectly: function() {
 
-    clearInterval(timer);
+    clearInterval(gameTimer);
 
-    game.correct++;
+    triviaGame.correctAnswerChoice++;
 
-    card.html("<h2>Correct!</h2>");
-    card.append("<img src='" + questions[game.currentQuestion].gif + "' />");
+    gameBody.html("<h2>You Are Correct!</h2>");
+    gameBody.append("<img src='" + questions[triviaGame.currentQuestion].gif + "' />");
 
-    if (game.currentQuestion === questions.length - 1) {
-      setTimeout(game.results, 3 * 1000);
+    if (triviaGame.currentQuestion === questions.length - 1) {
+      setTimeout(triviaGame.results, 3 * 1000);
     }
     else {
-      setTimeout(game.nextQuestion, 3 * 1000);
+      setTimeout(triviaGame.nextQuestion, 3 * 1000);
     }
   },
 
   reset: function() {
     this.currentQuestion = 0;
-    this.counter = countStartNumber;
-    this.correct = 0;
-    this.incorrect = 0;
+    this.counter = timerStart;
+    this.correctAnswerChoice = 0;
+    this.incorrectAnswerChoice = 0;
     this.loadQuestion();
   }
 };
@@ -174,14 +173,14 @@ var game = {
 // ON CLICK EVENTS FOR THE START OF THE TRIVIA
 
 $(document).on("click", "#start-over", function() {
-  game.reset();
+  triviaGame.reset();
 });
 
 $(document).on("click", ".answer-button", function(e) {
-  game.clicked(e);
+  triviaGame.clicked(e);
 });
 
 $(document).on("click", "#start", function() {
   $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
-  game.loadQuestion();
+  triviaGame.loadQuestion();
 });
